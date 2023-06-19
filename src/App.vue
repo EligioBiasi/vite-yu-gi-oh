52e082aa16764188ae5e9b10eab8cdfb59cb89eb
EligioBiasi <biasi.eligio@edufaimm.it>
2023-06-19 16:11:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppMain from './components/AppMain.vue'
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+    components:{
+      AppMain,
+      AppHeader,
+    }
+  }
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style>
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>
+            Yu-Gi-Oh Api
+        </h1>
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    header{
+        padding: 1rem;
+    }
+</style>
